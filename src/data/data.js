3d87d858e093baf4de276054675930eeebf4ba12
shibeSHIBEshibe <116483088+shibeSHIBEshibe@users.noreply.github.com>
2022-10-23 05:13:46 -0400

    Update data.js

--- src/data/data.js
+++ src/data/data.js
@@ -1,130 +1,113 @@
 const socialLinks = [
-  "https://github.com/vigneshshettyin",
-  "https://linkedin.com/in/vigneshshettyin",
-  "https://twitter.com/vigneshshettyin",
-  "https://instagram.com/vigneshshettyin",
-  "https://facebook.com/vigneshshettyin",
+
+  
+
 ];
+
 const profileData = {
-  userName: "vigneshshettyin",
-  photoLink: "https://github.com/vigneshshettyin.png",
-  desc: "Tech Geek | Mentor @ DeltaSJEC",
+
+  userName: "Oliver!",
+
+  photoLink: "https://i.imgur.com/YpLS7vt.jpg",
+
+  desc: "IT Dog",
+
 };
 
+
+
 const themeData = {
+
   light: {
+
     backgroundColor: "white",
+
     onHoverBackgroundColor: "#dfe6e9",
+
     cardBackgroundColor: "#ffffff",
+
     onHoverTextColor: "#636e72",
+
     footerColor: "black",
+
     footerSocialLinkColor: "white",
+
     headerFontColor: "#black",
+
     CardtextColor: "#2d3436",
+
   },
+
   dark: {
+
     backgroundColor: "#000000",
+
     onHoverBackgroundColor: "#02040a",
+
     cardBackgroundColor: "#222222",
+
     onHoverTextColor: "white",
+
     footerColor: "white",
+
     footerSocialLinkColor: "white",
+
     headerFontColor: "white",
+
     CardtextColor: "white",
+
   },
+
 };
+
 const linkData = [
+
   {
+
     id: 1,
-    link: "https://go.vigneshcodes.in/meetly",
-    name: "Meetly - Conferencing App",
+
+    link: "t.me/egghoe",
+
+    name: "Telegram",
+
     image:
-      "https://github.com/vigneshshettyin.png",
+
+      "https://imgur.com/a/lE4l4dX",
+
   },
+
   {
+
     id: 2,
-    link: "https://go.vigneshcodes.in/certify",
-    name: "Certify - Certification App",
+
+    link: "https://twitter.com/BorkedBeans",
+
+    name: "Twitter",
+
     image:
-      "https://play-lh.googleusercontent.com/QYc9QNf0_c-M7rhF4eo8daWbIRg_ukwzabR3Xj9TX_6zxG-AbzhKF5NS3CAd2X79fm0=s360-rw",
+
+      "https://imgur.com/a/7ZSIL7l",
+
   },
+
   {
+
     id: 3,
-    link: "https://eurl.tech",
-    name: "EatMyUrl - Free URL Shortener",
+
+    link: "https://www.bing.com/images/search?q=eggs&form=HDRSC2&first=1&tsc=ImageHoverTitle",
+
+    name: "My OnlyEggs",
+
     image:
-      "https://play-lh.googleusercontent.com/QYc9QNf0_c-M7rhF4eo8daWbIRg_ukwzabR3Xj9TX_6zxG-AbzhKF5NS3CAd2X79fm0=s360-rw",
+
+      "https://imgur.com/a/BAfpC9H",
+
+    
+
   },
-  {
-    id: 4,
-    link: "https://go.vigneshcodes.in/bulkmailer",
-    name: "BulkMailer - Free Bulk Mailer App",
-    image:
-      "https://play-lh.googleusercontent.com/QYc9QNf0_c-M7rhF4eo8daWbIRg_ukwzabR3Xj9TX_6zxG-AbzhKF5NS3CAd2X79fm0=s360-rw",
-  },
-  {
-    id: 5,
-    link: "https://tesla-clone-6820b.web.app",
-    name: "Tesla UI Clone Using ReactJS",
-    image:
-      "https://play-lh.googleusercontent.com/QYc9QNf0_c-M7rhF4eo8daWbIRg_ukwzabR3Xj9TX_6zxG-AbzhKF5NS3CAd2X79fm0=s360-rw",
-  },
-  {
-    id: 6,
-    link: "https://web-scrapper-v2.herokuapp.com",
-    name: "Free S.E.O Scrapper / API",
-    image:
-      "https://play-lh.googleusercontent.com/QYc9QNf0_c-M7rhF4eo8daWbIRg_ukwzabR3Xj9TX_6zxG-AbzhKF5NS3CAd2X79fm0=s360-rw",
-  },
-  {
-    id: 7,
-    link: "https://fervent-cray-ead198.netlify.app",
-    name: "Free Blogging Application",
-    image:
-      "https://play-lh.googleusercontent.com/QYc9QNf0_c-M7rhF4eo8daWbIRg_ukwzabR3Xj9TX_6zxG-AbzhKF5NS3CAd2X79fm0=s360-rw",
-  },
-  {
-    id: 8,
-    link: "https://vignesh-blog.herokuapp.com",
-    name: "Free Blogging Application",
-    image:
-      "https://play-lh.googleusercontent.com/QYc9QNf0_c-M7rhF4eo8daWbIRg_ukwzabR3Xj9TX_6zxG-AbzhKF5NS3CAd2X79fm0=s360-rw",
-  },
-  {
-    id: 9,
-    link: "https://quizzical-pike-e7098a.netlify.app",
-    name: "Open-Source React",
-    image:
-      "https://play-lh.googleusercontent.com/QYc9QNf0_c-M7rhF4eo8daWbIRg_ukwzabR3Xj9TX_6zxG-AbzhKF5NS3CAd2X79fm0=s360-rw",
-  },
-  {
-    id: 10,
-    link: "https://linktree.vigneshcodes.in",
-    name: "Display Links Uniquely ",
-    image:
-      "https://play-lh.googleusercontent.com/QYc9QNf0_c-M7rhF4eo8daWbIRg_ukwzabR3Xj9TX_6zxG-AbzhKF5NS3CAd2X79fm0=s360-rw",
-  },
-  {
-    id: 11,
-    link: "https://blog.vigneshcodes.in/quick-guide-to-deploy-using-docker",
-    name: "Quick Guide To Deploy Using Docker",
-    image:
-      "https://play-lh.googleusercontent.com/QYc9QNf0_c-M7rhF4eo8daWbIRg_ukwzabR3Xj9TX_6zxG-AbzhKF5NS3CAd2X79fm0=s360-rw",
-  },
-    {
-    id: 12,
-    link: "https://blog.vigneshcodes.in/create-linktree-clone",
-    name: "Create Linktree Clone",
-    image:
-      "https://play-lh.googleusercontent.com/QYc9QNf0_c-M7rhF4eo8daWbIRg_ukwzabR3Xj9TX_6zxG-AbzhKF5NS3CAd2X79fm0=s360-rw",
-  },
-  {
-    id: 13,
-    link: "https://blog.vigneshcodes.in/docker-for-production-build",
-    name: "Docker For Production Build",
-    image:
-      "https://play-lh.googleusercontent.com/QYc9QNf0_c-M7rhF4eo8daWbIRg_ukwzabR3Xj9TX_6zxG-AbzhKF5NS3CAd2X79fm0=s360-rw",
-  },
+
 ];
+
+
 
 export { socialLinks, profileData, linkData, themeData };
